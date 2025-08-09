46738e997d65665532bf2d77f17c8b232a6e0d6c
mayursuthar02 <mickymec00@gmail.com>
2025-08-09 16:06:52 +0530

    Change frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
 import Layout from './components/Layout'
 
 const App = () => {
+  useEffect(() => {
+    localStorage.setItem('chakra-ui-color-mode', 'light');
+  }, []);
+
   return <Layout/>
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil'
 
@@ -17,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RecoilRoot>
       <ChakraProvider theme={theme}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <BrowserRouter>
             <App />
           </BrowserRouter>
