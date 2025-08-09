0fb1651b8759a255e26336c392a9e6979caa7483
mayursuthar02 <mickymec00@gmail.com>
2025-08-09 11:55:38 +0530

    Frontend

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,9 @@ app.use('/api/payments', paymentRoutes);
 // Middleware
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://fashion-fusion-ecommerce-beta.vercel.app/'
+}));
 app.use(cookieParser());
 
 
